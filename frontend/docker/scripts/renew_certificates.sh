9372bc647c29214829ecd0cb1e12975cc2e09177
Felipe Johnny <felipijohnny@outlook.com>
2024-06-19 18:06:24 -0300

    ajustando https para denuncia.amalfis.com.br

--- frontend/docker/scripts/renew_certificates.sh
+++ frontend/docker/scripts/renew_certificates.sh
@@ -6,6 +6,12 @@ sleep 10
 # Gerar o certificado inicial
 certbot --nginx -d denuncia.amalfis.com.br --non-interactive --agree-tos -m ti@amalfis.com.br
 
+# Verificar se o certificado foi gerado
+if [ ! -f /etc/letsencrypt/live/denuncia.amalfis.com.br/fullchain.pem ]; then
+    echo "Certificado não gerado. Tentando novamente..."
+    certbot --nginx -d denuncia.amalfis.com.br --non-interactive --agree-tos -m ti@amalfis.com.br
+fi
+
 # Renovar certificados a cada 12 horas
 while true; do
     certbot renew
