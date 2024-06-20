383e8a2d56df93e40dc21a84c4d97f9ddb75a9cc
rafaelrvs <rafaelvieira1333@gmail.com>
2024-06-20 17:52:46 -0300

    adicionando correção no modal

--- frontend/src/Modal/Modal.jsx
+++ frontend/src/Modal/Modal.jsx
@@ -2,7 +2,11 @@ import "./Modal.css"
 const Modal = ({texto,background}) =>{
 
     return(
+
         <p className="modal" style={{ background: `${background}` }}>{texto}</p>
+
+
+
     )
 }
 export default Modal;
