48af035ae1f834011c553bfcff67c594db634971
Felipe Johnny <felipijohnny@outlook.com>
2024-06-24 13:41:00 -0300

    aplicando ajustes nos arquivos nginx

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,18 @@ const app = require("./src/app.js");
 const dotenv= require("dotenv");
 dotenv.config()
  
-const PORT = process.env.PORTAPI;
+const PORT = 3000;
 
+const https = require('https');
+const fs = require('fs');
 
-app.listen(PORT, ()=>{ // usando o app.js para escutar o servidor 
+ const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/denuncia.amalfis.com.br/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/denuncia.amalfis.com.br/fullchain.pem')
+}; 
+
+const server = https.createServer(httpsOptions,app);
+
+server.listen(PORT, ()=>{ // usando o app.js para escutar o servidor 
     console.log("Servidor ligado na porta " + PORT);
 });
